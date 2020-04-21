394e03ecebebf5880fc86dfe2760ae2c7bffd021
Glitch (hello-express) <none>
2020-04-21 17:09:20 +0000

    😬🍝 Checkpoint ./public/index.js:2307214/419

--- public/index.js
+++ public/index.js
@@ -1,18 +1,6 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-console.log("hello world :o");
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
-
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
-}
 
 // fetch the initial list of dreams
 fetch("/dreams")
