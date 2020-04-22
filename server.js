251618a7fd162860c3ff103d2697067d17c5e4c5
Glitch (hello-express) <none>
2020-04-22 13:21:11 +0000

    🐧🚥 Checkpoint ./server.js:2307214/1001

--- server.js
+++ server.js
@@ -5,13 +5,10 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const OpenTok = require('opentok');
+const opentok = new OpenTok(process.env.TOKBOX_KEY, process.env.TOKBOX_SECRET);
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+let sessions = {}
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -22,10 +19,19 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/room/:name", (request, response) => {
+app.get("/room/:room", (request, response) => {
   // express helps us take JS objects and send them as JSON
   response.sendFile(__dirname + "/views/room.html");
+});
+
+app.post("/room/:room", (request, response) => {
+  // express helps us take JS objects and send them as JSON
+  
+});
+
+app.get("/room/:room/:name", (request, response) => {
+  // express helps us take JS objects and send them as JSON
+  response.sendFile(__dirname + "/views/you.html");
 });
 
 // listen for requests :)
