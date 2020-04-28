621083b92a697e8b29987bb6a8ad7115d3425c60
Glitch (hello-express) <none>
2020-04-28 12:55:03 +0000

    🍄💫 Checkpoint ./public/you.js:2307214/6485 ./views/you.html:2307214/147

--- public/you.js
+++ public/you.js
@@ -2,7 +2,9 @@
 fetch(window.location.pathname, { method: "POST" })
   .then(response => response.json()) // parse the JSON from the server
   .then(response => {
-    var session = OT.initSession(response.key, response.sessionId);
+    let start = document.getElementById("start")
+    start.addEventListener("click", () => {
+      var session = OT.initSession(response.key, response.sessionId);
     let name = window.location.pathname.split("/").pop();
 
     session.on("streamCreated", function(event) {
@@ -62,4 +64,71 @@ fetch(window.location.pathname, { method: "POST" })
         session.publish(camPublisher, console.log);
       }
     });
+    })
+  
+    let audio = document.getElementById("audio")
+    audio.addEventListener("click", () => {
+      var session = OT.initSession(response.key, response.sessionId);
+    let name = window.location.pathname.split("/").pop();
+
+    session.on("streamCreated", function(event) {
+      if (event.stream.videoType == "screen") {
+        session.subscribe(
+          event.stream,
+          "subscriber",
+          {
+            insertMode: "append",
+            width: "auto",
+            height: "100%"
+          },
+          console.log
+        );
+      } else {
+        session.subscribe(
+          event.stream,
+          "publisher",
+          {
+            insertMode: "append",
+            width: "400px"
+          },
+          console.log
+        );
+      }
+    });
+
+    // Create a publisher
+    var screenPublisher = OT.initPublisher(
+      "publisher",
+      {
+        insertMode: "append",
+        width: "400px",
+        videoSource: "screen",
+        name: `${name} - <a href="${window.location.pathname}/screen" target="_blank">${window.location.pathname}/screen</a>`
+      },
+      console.log
+    );
+      
+      
+
+    var audioPublisher = OT.initPublisher(
+      "publisher",
+      {
+        insertMode: "append",
+        width: "400px",
+        name: `${name} - <a href="${window.location.pathname}/cam" target="_blank">${window.location.pathname}/cam</a>`
+      },
+      console.log
+    );
+
+    // Connect to the session
+    session.connect(response.token, function(error) {
+      // If the connection is successful, initialize a publisher and publish to the session
+      if (error) {
+        console.log(error);
+      } else {
+        session.publish(screenPublisher, console.log);
+        session.publish(audioPublisher, console.log);
+      }
+    });
+    })
   });
