cf07a2ee6a90876efbf65b4f7a5c04177dc51604
Glitch (hello-express) <none>
2020-06-03 15:29:13 +0000

    🎂😂 Checkpoint ./server.js:2307214/4007

--- server.js
+++ server.js
@@ -72,111 +72,23 @@ app.post("/record/:id/start", (request, response) => {
   }
 });
 
-app.get("/room/:room/:name/screen", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.sendFile(__dirname + "/views/screen.html");
-});
-
-app.get("/room/:room/:name/cam", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.sendFile(__dirname + "/views/cam.html");
-});
-
-app.post("/room/:room/:name/screen", (request, response) => {
-  if (sessions[request.params.room]) {
-    response.json({
-      sessionId: sessions[request.params.room],
-      token: opentok.generateToken(sessions[request.params.room], {
-        role: "subscriber",
-        expireTime: new Date().getTime() / 1000 + 7 * 24 * 60 * 60 // in one week
-      }),
-      key: process.env.TOKBOX_KEY
-    });
-  } else {
-    opentok.createSession(
-      { mediaMode: "routed", archiveMode: "always" },
-      (err, session) => {
-        if (err) return console.log(err);
-
-        // save the sessionId
-        sessions[request.params.room] = session.sessionId;
-        response.json({
-          sessionId: sessions[request.params.room],
-          token: session.generateToken({
-            role: "subscriber",
-            expireTime: new Date().getTime() / 1000 + 7 * 24 * 60 * 60 // in one week
-          }),
-          key: process.env.TOKBOX_KEY
-        });
-      }
-    );
-  }
-});
-
-app.post("/room/:room/:name/cam", (request, response) => {
-  if (sessions[request.params.room]) {
-    response.json({
-      sessionId: sessions[request.params.room],
-      token: opentok.generateToken(sessions[request.params.room], {
-        role: "subscriber",
-        expireTime: new Date().getTime() / 1000 + 7 * 24 * 60 * 60 // in one week
-      }),
-      key: process.env.TOKBOX_KEY
-    });
-  } else {
-    opentok.createSession(
-      { mediaMode: "routed", archiveMode: "always" },
-      (err, session) => {
-        if (err) return console.log(err);
-
-        // save the sessionId
-        sessions[request.params.room] = session.sessionId;
-        response.json({
-          sessionId: sessions[request.params.room],
-          token: session.generateToken({
-            role: "subscriber",
-            expireTime: new Date().getTime() / 1000 + 7 * 24 * 60 * 60 // in one week
-          }),
-          key: process.env.TOKBOX_KEY
-        });
-      }
-    );
-  }
-});
-
-app.get("/room/:room/:name", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.sendFile(__dirname + "/views/you.html");
-});
-
-app.post("/room/:room/:name", (request, response) => {
-  if (sessions[request.params.room]) {
-    response.json({
-      sessionId: sessions[request.params.room],
-      token: opentok.generateToken(sessions[request.params.room], {
-        role: "publisher",
-        expireTime: new Date().getTime() / 1000 + 7 * 24 * 60 * 60,
-        data: `name=${request.params.name}`
-      }),
-      key: process.env.TOKBOX_KEY
-    });
-  } else {
-    opentok.createSession(
-      { mediaMode: "routed", archiveMode: "always" },
-      (err, session) => {
-        if (err) return console.log(err);
-
-        // save the sessionId
-        sessions[request.params.room] = session.sessionId;
-        response.json({
-          sessionId: sessions[request.params.room],
-          token: session.generateToken({
-            role: "publisher",
-            expireTime: new Date().getTime() / 1000 + 7 * 24 * 60 * 60,
-            data: `name=${request.params.name}`
-          }),
-          key: process.env.TOKBOX_KEY
-        });
+app.post("/record/:id/stop", (request, response) => {
+  if (sessions[request.params.id]) {
+    opentok.startArchive(
+      sessions[request.params.id],
+      {
+        name: request.params.id
+      },
+      (err, archive) => {
+        if (err)
+          return response.send(
+            500,
+            "Could not start archive for session " +
+              request.params.id +
+              ". error=" +
+              err.message
+          );
+        response.json(archive);
       }
     );
   }
