d788e43021716eb34a866dc9622f2856524a1626
Paul Ardeleanu <paul.ardeleanu@vonage.com>
2020-06-17 15:53:20 +0100

    Google authentication

--- public/index.js
+++ public/index.js
@@ -1,13 +1,3 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-let form = document.getElementById("login")
-form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  
-  let room = form.elements.room.value
-  let you = form.elements.you.value
-  
-  window.location.href = `/room/${room}/${you}`
-  
-})
